ef9e73131e0e94ac8cfbd9c0403f3f331b59212b
James McKinney <james@slashpoundbang.com>
2014-09-14 20:43:39 -0400

    install citizen_budget_model

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Readyreckoner
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.available_locales = ['en-CA', 'fr-CA']
+    config.i18n.default_locale = 'en-CA'
+    config.i18n.fallbacks = {'en-CA' => ['en-CA', :en, 'fr-CA', :fr], 'fr-CA' => ['fr-CA', :fr, 'en-CA', :en]}
+    config.i18n.enforce_available_locales = false
   end
 end
