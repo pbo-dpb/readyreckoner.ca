59aa2b2c6b10f1b2f06813d26a38938bc940e47f
James McKinney <james@slashpoundbang.com>
2014-09-26 17:00:36 -0400

    set locale, add hreflang link tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_locale
+    session[:locale] = I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+  end
 end
