1f763b32c08ed531fff4fb39811488465287c189
James McKinney <james@slashpoundbang.com>
2014-09-26 14:50:29 -0400

    rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Readyreckoner
+module ReadyReckoner
   class Application < Rails::Application
     config.i18n.available_locales = ['en-CA', 'fr-CA']
     config.i18n.default_locale = 'en-CA'
