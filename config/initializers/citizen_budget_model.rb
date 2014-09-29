d5c993efb25d786b13ac1f042aecd0ae12c351aa
James McKinney <james@slashpoundbang.com>
2014-09-28 23:04:17 -0400

    configure citizen_budget_model hidden_fields

---
+++ config/initializers/citizen_budget_model.rb
@@ -0,0 +1,31 @@
+CitizenBudgetModel.hidden_fields = {
+  simulator: [
+    :equation,
+  ],
+  section: [
+    :description,
+    :popover,
+    :modal,
+  ],
+  question: [
+    :account,
+    # Widget
+    :widget,
+    :labels,
+    :options,
+    :revenue,
+    # Text
+    :title,
+    :description,
+    :modal,
+    :popover,
+    # HTML
+    :required,
+    :placeholder,
+    :maxlength,
+    :rows,
+    :cols,
+    :size,
+    :html_class,
+  ],
+}
