2ca9de386f0a0592c1e7cc248d0ab20245ee588f
James McKinney <james@slashpoundbang.com>
2014-09-30 15:20:15 -0400

    move engine to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   post '/print', to: 'pages#print', as: :print
-  mount CitizenBudgetModel::Engine => '/'
+  mount CitizenBudgetModel::Engine => '/admin'
 end
