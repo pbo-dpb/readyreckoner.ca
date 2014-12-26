915b8ed12ca14a4d6ea4597b8205bbdb1855c4a0
James McKinney <james@slashpoundbang.com>
2014-12-26 00:13:29 -0500

    add coveralls gem

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'coveralls'
 end
 
 group :production do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+Rails.backtrace_cleaner.remove_silencers!
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -16,7 +18,7 @@ require 'rspec/rails'
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
