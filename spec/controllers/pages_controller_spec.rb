2af3207f49523a96f0473929b61f32952d55cbcd
James McKinney <james@slashpoundbang.com>
2014-12-26 00:03:21 -0500

    fix controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe PagesController, type: :controller do
       CitizenBudgetModel::Simulator.create!(organization_id: 1, name_en_ca: 'A')
       @active = CitizenBudgetModel::Simulator.create!(organization_id: 1, name_en_ca: 'Simulator', active: true)
       CitizenBudgetModel::Simulator.create!(organization_id: 1, name_en_ca: 'B')
+
+      section = CitizenBudgetModel::Section.create!(simulator: @active, title_en_ca: 'Section')
+      question = CitizenBudgetModel::Question.create!(section: section, minimum: 1, maximum: 5, step: 2, default_value: 3, unit_value: 100, name: 'Variable', machine_name: 'var')
     end
 
     describe 'GET index' do
@@ -22,7 +25,7 @@ RSpec.describe PagesController, type: :controller do
 
     describe 'GET print' do
       it 'returns http success' do
-        post :print, locale: I18n.default_locale
+        post :print, locale: I18n.default_locale, variables: {var: 5}
         expect(response).to be_success
         expect(assigns(:simulator)).to eq(@active)
         expect(response.header['Content-Disposition']).to eq('inline; filename="Ready_Reckoner_Report.pdf"')
